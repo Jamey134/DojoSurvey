86dac4c90d472ca57319737f846720623add1177
James Watson <jameywatson134@gmail.com>
2023-08-06 10:48:53 -0700

    update

--- Program.cs
+++ Program.cs
@@ -1,5 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
+// Add our service
+builder.Services.AddControllersWithViews();
 var app = builder.Build();
+
+// Our builder code
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthorization();
 
 app.MapGet("/", () => "Hello World!");
 
